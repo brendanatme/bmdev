19e92b83060988989f13e197d741030c69294994
brendanatme <registrations@brendanat.me>
2021-01-04 21:02:05 -0500

    feature: improve loading animation

--- src/components/page-transition/page-transition.component.js
+++ src/components/page-transition/page-transition.component.js
@@ -3,6 +3,8 @@ import * as layoutModel from '@/src/models/layout.model'
 import LogoSvg from '@/src/svgs/logo.svg'
 import styles from './page-transition.component.module.css'
 
+const text = ['L', 'O', 'A', 'D', 'I', 'N', 'G']
+
 const PageTransition = () => {
   const pageTransitionIsOpen = layoutModel.select.pageTransitionIsOpen()
   const initialLoad = layoutModel.select.initialLoad()
@@ -16,6 +18,11 @@ const PageTransition = () => {
     <>
       <div className={`${styles.bg} ${styles[`bg--${state}`]}`} />
       <LogoSvg className={`${styles.logo} ${styles[`logo--${state}`]} fixed m-auto inset-0`} />
+      <div className={`${styles.text} ${styles[`text--${state}`]} fixed m-auto top-24 inset-0`}>
+        {text.map((char, i) => (
+          <span className={`inline-block opacity-0 ${styles[`char${i}`]}`} key={char}>{char}</span>
+        ))}
+      </div>
     </>
   )
 }
