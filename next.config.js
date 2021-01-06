9bc9c08448c81e8afdd4702c401dd4d689eb6323
brendanatme <registrations@brendanat.me>
2021-01-06 17:55:49 -0500

    fix: configure netlify deployments

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  /**
+   * required for netlify deployments
+   * see https://github.com/netlify/next-on-netlify
+   */
+  target: 'serverless',
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
