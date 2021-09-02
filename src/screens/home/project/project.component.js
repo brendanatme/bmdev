2e592b6457d29fc6a48a483155d1c6768744be41
brendanatme <brendanm@hey.com>
2021-09-02 09:53:13 -0400

    refactor: home page project hover styles

--- src/screens/home/project/project.component.js
+++ src/screens/home/project/project.component.js
@@ -10,10 +10,11 @@ const ProjectThumb = ({ project }) => {
           <Picture className={styles.bg} image={project.images.banner} />
           <div className={`fill ${styles.overlay}`} />
           <div className={`absolute bottom-0 left-0 py-2 px-3 md:py-5 md:px-10 w-full`}>
-            <div className={`${styles.metaBg} bg-gray-50 fill`} />
-            <div className={`text-gray-700 text-display-1 my-1 md:my-2 flex-1 leading-none ${styles.name}`}>{project.name}</div>
-            <div className={`text-gray-700 text-display-2 my-1 md:my-2 flex-initial ${styles.client}`}>{project.thumbClient}</div>
-            <div className={`text-gray-700 text-display-2 my-1 md:my-2 flex-initial ${styles.date}`}>{project.date}</div>
+            <div className={`fill block md:hidden ${styles.metaBg}`} />
+            <div className={`hidden md:block w-32 bg-gray-50 absolute top-1 left-3 md:top-3 md:left-10 ${styles.line}`} />
+            <div className={`text-gray-50 text-display-1 my-1 md:my-2 flex-1 leading-none ${styles.name}`}>{project.name}</div>
+            <div className={`text-gray-50 text-display-2 my-1 md:my-2 flex-initial ${styles.client}`}>{project.thumbClient}</div>
+            <div className={`text-gray-50 text-display-2 my-1 md:my-2 flex-initial ${styles.date}`}>{project.date}</div>
           </div>
         </div>
       </Link>
